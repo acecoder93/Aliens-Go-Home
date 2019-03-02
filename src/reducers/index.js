19b662f65dd19b391aa3595224005b365f6b2993
Anuj <anujsaheba@gmail.com>
2019-03-02 17:03:27 -0600

    setup redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    message: `It's easy to integgrate React and Redux, isn't it?`
+};
+
+function reducer( state = initialState){
+    return state;
+
+}
+
+export default reducer;
